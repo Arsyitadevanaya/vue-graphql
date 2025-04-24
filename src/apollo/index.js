ec6ab2bc9768b1dcdddde8a3566dda1ed52aa507
Arsyitadevanaya <arsyitadevanaya@gmail.com>
2025-04-24 21:22:37 +0700

    fix:env ignore

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -9,7 +9,7 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsImlzcyI6Imh0dHBzOi8vb3JpZW5zc3RhZ2luZy5vcmllbnMubXkuaWQ6MzUwMCIsImlhdCI6MTc0NDYxMTk4MiwibmJmIjoxNzQ0NjExOTgyLCJqdGkiOiI1OTUxYjgyYS04MGZlLTQ3Y2YtOTE3NC1jNTRlMWJlN2NmYTgiLCJleHAiOjE3NDQ2OTgzODJ9.39ym6x-nL7YY-2MV08bB4Tvq21iIIToQFbKy7e5OgEY`,
+      Authorization:import.meta.env.API_KEY ? `Bearer ${import.meta.env.API_KEY}` : "",
     }
   }
 })
